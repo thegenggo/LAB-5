901e259be3c5e05c67bed1d8f4a31bebee83c821
thegenggo <66797373+thegenggo@users.noreply.github.com>
2020-12-15 15:51:28 +0700

    Update lab5_2.cpp

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <cmath>
+#define _USE_MATH_DEFINES
 
 using namespace std;
+
+double deg2rad(double x)
+{
+    return x*M_PI/180.0;
+}
+
+double rad2deg(double x)
+{
+    return x*180.0/M_PI;
+}
+
+double findXComponent(double x,double y,double xAngle,double yAngle)
+{
+    return x*cos(xAngle)+y*cos(yAngle);
+}
+
+double findYComponent(double x,double y,double xAngle,double yAngle)
+{
+    return x*sin(xAngle)+y*sin(yAngle);
+}
+
+double pythagoras(double x,double y)
+{
+    return sqrt(pow(x,2)+pow(y,2));
+}
+
+void showResult(double x, double y)
+{
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << x << endl;
+    cout << "Direction of the resultant vector (deg) = " << y << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
